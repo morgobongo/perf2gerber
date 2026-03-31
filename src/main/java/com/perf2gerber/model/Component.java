922efca73a41238f9d41147712f453e09bf798bd
Morgo <morgan.jacob14@gmail.com>
2026-03-30 23:27:13 -0400

    ajustements

--- src/main/java/com/perf2gerber/model/Component.java
+++ src/main/java/com/perf2gerber/model/Component.java
@@ -21,38 +21,93 @@ public abstract class Component {
         this.id = UUID.randomUUID().toString();
     }
 
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
-    
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
-    
-    public String getValue() { return value; }
-    public void setValue(String value) { this.value = value; }
-    
-    public String getType() { return type; }
-    public void setType(String type) { this.type = type; }
-    
-    public String getColor() { return color; }
-    public void setColor(String color) { this.color = color; }
-    
-    public int getStartX() { return startX; }
-    public void setStartX(int startX) { this.startX = startX; }
-    
-    public int getStartY() { return startY; }
-    public void setStartY(int startY) { this.startY = startY; }
-    
-    public double getRotation() { return rotation; }
-    public void setRotation(double rotation) { this.rotation = rotation; }
-    
-    public boolean isShowName() { return showName; }
-    public void setShowName(boolean showName) { this.showName = showName; }
-    
-    public boolean isShowValue() { return showValue; }
-    public void setShowValue(boolean showValue) { this.showValue = showValue; }
+    public String getId() {
+        return id;
+    }
 
-    public boolean isHovered() { return isHovered; }
-    public void setHovered(boolean hovered) { this.isHovered = hovered; }
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getStartX() {
+        return startX;
+    }
+
+    public void setStartX(int startX) {
+        this.startX = startX;
+    }
+
+    public int getStartY() {
+        return startY;
+    }
+
+    public void setStartY(int startY) {
+        this.startY = startY;
+    }
+
+    public double getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(double rotation) {
+        this.rotation = rotation;
+    }
+
+    public boolean isShowName() {
+        return showName;
+    }
+
+    public void setShowName(boolean showName) {
+        this.showName = showName;
+    }
+
+    public boolean isShowValue() {
+        return showValue;
+    }
+
+    public void setShowValue(boolean showValue) {
+        this.showValue = showValue;
+    }
+
+    public boolean isHovered() {
+        return isHovered;
+    }
+
+    public void setHovered(boolean hovered) {
+        this.isHovered = hovered;
+    }
 
     /**
      * Determine if a grid coordinate falls within the boundary.
@@ -62,7 +117,8 @@ public abstract class Component {
 
     /**
      * Creates a deep duplicate of this component (must be implemented by subtypes).
-     * The ID generated for the new component is freshly randomized on instantiation.
+     * The ID generated for the new component is freshly randomized on
+     * instantiation.
      */
     public abstract Component cloneComponent();
 }
